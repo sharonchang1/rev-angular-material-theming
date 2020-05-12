875673acc6df428d0875d62e5281c08c706abdce
sharonchang1 <13601378+sharonchang1@users.noreply.github.com>
2020-05-12 10:58:07 -0600

    Giant commit of many things -- buttons, inputs, dropdowns, radio buttons, and checkboxes implemented in cards. Also added the Foundation button highlight

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+interface Car {
+  value: string;
+  viewValue: string;
+}
+
+interface DropdownOptions {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +24,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-material-theme';
+
+  selectedValue: string;
+    selectedCar: string;
+
+    dropdownOptions: DropdownOptions[] = [
+      {value: 'option-1', viewValue: 'Option 1'},
+      {value: 'option-2', viewValue: 'Option 2'},
+      {value: 'option-3', viewValue: 'Option 3'}
+    ];
+
+    selectionOptions: DropdownOptions[] = [
+      {value: 'unselected', viewValue: 'Unselected'},
+      {value: 'selected', viewValue: 'Selected'},
+      {value: 'disabled', viewValue: 'Disabled'}
+    ];
+
+    foods: Food[] = [
+      {value: 'steak-0', viewValue: 'Steak'},
+      {value: 'pizza-1', viewValue: 'Pizza'},
+      {value: 'tacos-2', viewValue: 'Tacos'}
+    ];
+
+    cars: Car[] = [
+      {value: 'volvo', viewValue: 'Volvo'},
+      {value: 'saab', viewValue: 'Saab'},
+      {value: 'mercedes', viewValue: 'Mercedes'}
+    ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,44 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatListModule } from '@angular/material/list';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    BrowserAnimationsModule,
+
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+
+    MatFormFieldModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
